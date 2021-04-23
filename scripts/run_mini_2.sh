05547d62d1fd3f4a9686886c5bc78208c989958a
asrafulashiq <asrafulashiq@gmail.com>
2021-04-23 16:40:22 -0400

    update

--- scripts/run_mini_2.sh
+++ scripts/run_mini_2.sh
@@ -12,7 +12,7 @@ SLURM="--nodes ${nodes} --ngpus ${gpus}"
 
 cmd="python run_with_submitit.py ${SLURM} \
     --model deit_tiny_patch16_224 --batch-size 256 --output_dir outputs/deit_tiny_mini_less \
-    --data-set mini-IN  --job_name deit_tiny_mini_less ----no-model-ema \
+    --data-set mini-IN  --job_name deit_tiny_mini_less --no-model-ema \
     --warmup-epochs 30 --cooldown-epochs 40 --patience-epochs 60 --check_val_every_n_epoch 5 \
     --epochs 2000 --eval-type few_shot --num_episodes 200"
 
